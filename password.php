45cd18d66144172f3e26e8de8d63852032a6c846
GODSKADI <aidrean.gf@gmail.com>
2018-12-14 19:08:25 +0100

    recuperacion de contraseñas

---
+++ password.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<?php
+function connectar(){
+  //connexió dins block try-catch:
+  //  prova d'executar el contingut del try
+  //  si falla executa el catch
+  try {
+    $hostname = "localhost";
+    $dbname = "ScrumDBfinal";
+    $username = "Administrador";
+    $pw = "P@ssw0rd";
+    $pdo = new PDO ("mysql:host=$hostname;dbname=$dbname","$username","$pw");
+  } catch (PDOException $e) {
+    echo "Failed to get DB handle: " . $e->getMessage() . "\n";
+    exit;
+  }
+  return $pdo;
+}
+?>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="estiloScrum.css">
+	<title>Recuperación de la contraseña</title>
+	<script type="text/javascript" src="scripts.js"></script>
+	<!-- Materialize -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body style="padding: 2%;">
+
+<?php
+  $pdo = connectar();
+  $titulo = "Recuperación de la contraseña";
+  if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $correo = $_POST["email"];
+  }
+
+
+	echo "<div class='Login-Style'>";
+	echo "<h2 class='h2-Style'>Recuperarción</h2>";
+  echo "<h2 class='h2-Style'>de la contraseña</h2>";
+	echo "<form class='formulario' action='contraseña.php' method='POST' align='center'>";
+		echo"<label class='Label-Style'>Nueva Contraseña: </label>";
+		echo "<input type='password' name='contraseña'><br>";
+    echo"<label class='Label-Style'>Repite la Contraseña: </label>";
+    echo "<input type='password' name='contraseña'><br>";
+		echo "<input type='submit' value='Enviar' name='submit' id='btn' class='waves-effect waves-light btn-small'><br>";
+	echo "</form>";
+	echo "</div>";
+?>
+</body>
+</html>
