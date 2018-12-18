6199e1ac2eddd969291448bed117441cedabf1c4
ncristiansa <ncristiansalinasandia@gmail.com>
2018-12-18 02:59:56 +0100

    Añade p pero debe crear tambien en sprint 2

--- functions.js
+++ functions.js
@@ -1,9 +1,6 @@
 if(tipo==2 || tipo==3){
 	showinfoProject();
 	showSprintInfo();
-	showHomeworks();
-
-
 }
 
 function insertAfter(e,i){
@@ -120,6 +117,21 @@ function showSprintInfoOneByOne(Position){
 		}
 		
 	}
+	if(tipo==2){
+		var AddButton = document.createElement("input");
+		AddButton.setAttribute("id", "buttonAdd");
+		var Task = document.createElement("input");
+		AddButton.addEventListener("click", addTask);
+		AddButton.setAttribute("type", "image");
+		AddButton.setAttribute("src", "images/add.jpeg");
+		AddButton.setAttribute("width", "25");
+		AddButton.setAttribute("height", "25");
+		AddButton.setAttribute("align", "right");
+
+		Task.setAttribute("type", "text");
+		divPSprint.appendChild(AddButton);
+		divPSprint.appendChild(Task);
+	}
 
 			
 	elementdiv.appendChild(divSprint);			
@@ -142,55 +154,15 @@ function showSprintInfo(){
 		} 
 	}
 }
-function showHomeworks(){
-	var elementh2 = document.getElementsByTagName("h2")[0];
-	var elementDiv = document.createElement("div");
-	var titleh2 = document.createElement("h2");
-	var texth2 = document.createTextNode("Especificaciones");
-	titleh2.appendChild(texth2);
-	var AddButton = document.createElement("input");
-	AddButton.setAttribute("id", "buttonAdd");
-	var Task = document.createElement("input");
-
-	AddButton.addEventListener("click", addTask);
-	AddButton.addEventListener("click", checkInputEmpty);
-	var TaskUL = document.createElement("ul");
-	
-	var count = 0;
-	while(count!=descriptionsJS.length){
-		var newLi = document.createElement("li");
-		var newText = document.createTextNode(descriptionsJS[count]);
-		newLi.appendChild(newText);
-		TaskUL.appendChild(newLi);
-		count=count+1;
-	}
-	elementDiv.setAttribute("class", "info-project");
-	titleh2.setAttribute("class", "titleh2-project");
-
-	AddButton.setAttribute("type", "image");
-	AddButton.setAttribute("src", "images/add.jpeg");
-	AddButton.setAttribute("width", "25");
-	AddButton.setAttribute("height", "25");
-	AddButton.setAttribute("align", "right");
-
-	Task.setAttribute("type", "text");
-	Task.setAttribute("class", "task-text");
-
-	elementDiv.appendChild(titleh2);
-	elementDiv.appendChild(TaskUL);
-	elementDiv.appendChild(AddButton);
-	elementDiv.appendChild(Task);
-
-	insertAfter(elementh2, elementDiv);
-}
 
 function addTask(){
 
-	var myListLI = document.getElementsByTagName("ul")[0];
+	var myListP = document.getElementsByClassName("OneHomework")[2];
 	var contentTask = document.getElementsByTagName("input")[1].value;
-	var newContentLi = document.createElement("li");
+	var newContentP = document.createElement("p");
+	newContentP.setAttribute("class", "OneHomework");
 	var newContentTask = document.createTextNode(contentTask);
-	newContentLi.appendChild(newContentTask);
-	myListLI.appendChild(newContentLi);
+	newContentP.appendChild(newContentTask);
+	insertAfter(myListP, newContentP);
 
 }	
