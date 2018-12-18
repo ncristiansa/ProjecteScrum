979f44a02d8f735e52fcbb01198067273f914cf4
ncristiansa <ncristiansalinasandia@gmail.com>
2018-12-18 11:20:09 +0100

    change

---
+++ Administration.php
@@ -0,0 +1,185 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="estiloScrum.css">
+	<script type="text/javascript" defer src="functions.js"></script>
+	<title>Administración de Projecto</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+<?php
+	/*
+		Inicio esta variable para usarla más adelante almacenar el resultado de una consulta.
+	*/
+	$NameUser = "";
+	
+	$nameusr = $_SESSION["Name"];
+	
+	$server = "localhost";
+ 	$user = "Administrador";
+ 	$pass = "P@ssw0rd";
+ 	$bbdd = "ScrumDB3_2";
+ 	$connect = mysqli_connect($server,$user, $pass, $bbdd);
+ 	/*
+		En la variable $consulta lanzaremos nuestra pequeña consulta SQL
+		donde obtendremos el nombre del usuario donde el nickname sea igual
+		a nuestra variable $nameusr (ésta almacena la SESSION["Name"]).
+ 	*/
+ 	$consulta = ("SELECT username FROM Users WHERE nickname='$nameusr';");
+ 	/*
+		Almacenaremos el anterior resultado en nuestra variable, valga la
+		redundancia, llamada resultado.
+ 	*/
+ 	$resultado = mysqli_query($connect, $consulta);
+ 	/*
+		Finalmente comprobaremos si tenemos algún resultado.
+		En caso de que haya un resultado, en este caso sí,
+		éste lo guardaremos en nuestra variable $NameUser donde almacenará
+		$variable = $registro["columna"], es decir $NameUser = $registro["username"]. El nombre username es el que tenemos en nuestra tabla Users donde se guarda el nombre del usuario.
+ 	*/
+	if($registro = mysqli_fetch_assoc($resultado)){
+		$NameUser = $registro["username"];
+	}
+	/*
+		Obtendré la URL por el = para otbtener la posición 1 que es el nombre
+	*/
+		
+?>
+<?php
+	echo"<nav>";
+		echo"<ul>";
+			echo"<li class='lihorizontal'>";
+				echo"<img class='imgusuario' src='images\usericon.png'>";
+				
+			echo"</li>";
+			echo"<li class='liimglogout'>";
+?>
+				<a href='vistainicial.php?exituser=true'>
+<?php
+				echo"<img class='imglogout' src='images\logout.png'>";
+?>
+				</a>
+<?php
+				print_r($NameUser);
+			echo"</li>";
+		echo"</ul>";
+	echo"</nav>";
+?>
+
+
+<?php
+		$host=$_SERVER["HTTP_HOST"];
+		$url= $_SERVER["REQUEST_URI"];
+		/*
+			Separo la url mediante el = 
+		*/
+		
+		$cutnewUrl = explode("%20", $url);
+		$newutl = explode("=", $url);
+		$ProjectName = $newutl[1];
+		$NameProject = str_replace("%20", " ", $ProjectName);
+		
+		/*
+			Al obtener el nombre del projecto mediante el método GET puedo realizar una consulta que me devuelva los datos de ese proyecto y enviarselo mediante un array al JS para mostrar con DOM toda la información de X projecto.
+		*/
+		$InfoProject = ("SELECT * FROM Projects WHERE nameProject='$NameProject';");
+		$resultInfoProject = mysqli_query($connect, $InfoProject);
+		$nameInfoProject = [];
+		$descriptionInfoProject = [];
+		$scrumMasterInfoProject = [];
+		$productOwnerInfoProject = [];
+		while ($info = mysqli_fetch_assoc($resultInfoProject)) {
+			$NameP = $info["nameProject"];
+			$idProject=$info["projectID"];
+			$DescripcionP = $info["description"];
+			$scrumMasternameP = $info["scrumMasterName"];
+			$productOwnernameP = $info["productOwnerName"];
+			array_push($nameInfoProject, $NameP);
+			array_push($descriptionInfoProject, $DescripcionP);
+			array_push($scrumMasterInfoProject, $scrumMasternameP);
+			array_push($productOwnerInfoProject, $productOwnernameP);
+		}
+		$typeUser = ("SELECT type FROM Users WHERE username='$NameUser';");
+		$resultTypeUser = mysqli_query($connect, $typeUser);
+		if($Query = mysqli_fetch_assoc($resultTypeUser)){
+			$userType = $Query["type"];
+		}
+		
+		//Con el id del projecto buscamos los sprints de este, creando una array de arrays, para luego enviarla a javascript
+		$SprintsInfo = ("SELECT * FROM Sprints WHERE projectID=$idProject order by orderNumber;");
+		$resultSprints = mysqli_query($connect,$SprintsInfo);
+		$finalSprintInfoArray = [];
+		$restartSprintInfoArray = [];
+		while ($info = mysqli_fetch_assoc($resultSprints)) {
+			$order=$info["orderNumber"];
+			$hours=$info["hours"];
+			$startDate=$info["startDate"];
+			$endDate=$info["endDate"];
+			$status=$info["status"];
+			$sprintID=$info["sprintID"];
+			array_push($restartSprintInfoArray, $order);
+			array_push($restartSprintInfoArray, $hours);
+			array_push($restartSprintInfoArray, $startDate);
+			array_push($restartSprintInfoArray, $endDate);
+			array_push($restartSprintInfoArray, $status);
+			array_push($restartSprintInfoArray, $sprintID);
+			array_push($finalSprintInfoArray, $restartSprintInfoArray);			
+			$restartSprintInfoArray=[];
+		}
+		//Con los ids de los sprints del projecto buscamos las especificaciones de este, creando una array de arrays, para luego enviarla a javascript
+		$HomeworkInfo = ("SELECT * FROM Homework WHERE sprintID IN (SELECT  sprintID FROM Sprints WHERE projectID='$idProject' order by orderNumber) ORDER BY orderHW;");
+		$HomeworkResult = mysqli_query($connect,$HomeworkInfo);
+		$finalHWInfoArray=[];
+		$restartHWInfoArray=[];
+		while ($info = mysqli_fetch_assoc($HomeworkResult)){
+			$homeworkID=$info["homeworkID"];
+			$description=$info["description"];
+			$hours=$info["hours"];
+			$sprintID=$info["sprintID"];
+			$orderHW=$info["orderHW"];
+			array_push($restartHWInfoArray, $homeworkID);
+			array_push($restartHWInfoArray, $description);
+			array_push($restartHWInfoArray, $hours);
+			array_push($restartHWInfoArray, $sprintID);
+			array_push($restartHWInfoArray, $orderHW);
+			array_push($finalHWInfoArray, $restartHWInfoArray);		
+			$restartHWInfoArray=[];
+		}
+?>
+
+
+<?php
+	/*
+		Hemos creado una función llamada destroySession para que una vez sea llamada destruya la SESSION actual y nos redirija a login.php
+	*/
+	function destroySession(){
+		session_destroy();
+		header("Location: login.php");
+	}
+	/*
+		Esta condición nos permite saber si el usuario ha hecho click en la imagen donde hemos añadido una especie de
+		variable que estará siempre en True, activada para que cuando se haya hecho clic llame a la función destroySession.
+	*/
+	if(isset($_GET['exituser'])){
+		destroySession();
+	}
+?>
+<script type="text/javascript">
+	var tipo = '<?php echo $userType;?>';
+</script>
+<script type="text/javascript">
+	var nameProjectJS = <?php echo json_encode($nameInfoProject);?>;
+	var descriptionProjectJS = <?php echo json_encode($descriptionInfoProject);?>;
+	var scrumMasternameJS = <?php echo json_encode($scrumMasterInfoProject);?>;
+	var productOwnernameJS = <?php echo json_encode($productOwnerInfoProject);?>;
+	var arraySprint = <?php echo json_encode($finalSprintInfoArray);?>;
+	var arrayHW = <?php echo json_encode($finalHWInfoArray);?>;
+</script>
+
+	
+
+</body>
+</html>
---
+++ functions.js
@@ -0,0 +1,166 @@
+if(tipo==2 || tipo==3){
+	showinfoProject();
+	showSprintInfo();
+}
+
+function insertAfter(e,i){
+	if(e.nextSibling){
+		e.parentNode.insertBefore(i,e.nextSibling);
+
+	} else {
+		e.parentNode.appendChild(i);
+	}
+}
+function showinfoProject(){
+	var elementNav = document.getElementsByTagName("nav")[0];
+	var divInfo = document.createElement("div");
+	var titleH2 = document.createElement("h2");
+	
+		var textH2 = document.createTextNode(nameProjectJS[0]);
+		titleH2.appendChild(textH2);
+
+	var nameP = document.createElement("p");
+
+		var textP = document.createTextNode("Nombre: "+nameProjectJS[0]);
+		nameP.appendChild(textP);
+
+	
+	var descripP = document.createElement("p");
+		
+		var textP = document.createTextNode("Descripción: "+descriptionProjectJS[0]);
+		descripP.appendChild(textP);
+
+	
+	var scrumnameP = document.createElement("p");
+
+		var textP = document.createTextNode("Scrum Master: "+scrumMasternameJS[0]);
+		scrumnameP.appendChild(textP);
+
+
+	var productownerP = document.createElement("p");
+
+		var textP = document.createTextNode("Product Owner: "+productOwnernameJS[0]);
+		productownerP.appendChild(textP);
+
+	/*
+		Añadimos clases a los elementos creandos anteriormente.
+	*/
+	divInfo.setAttribute("class", "info-project");
+	titleH2.setAttribute("class", "titleh2-project");
+
+	/*
+		Añadimos h2, p, br dentro de nuestro elemento DIV
+	*/
+	divInfo.appendChild(titleH2);
+	divInfo.appendChild(nameP);
+	divInfo.appendChild(descripP);
+	divInfo.appendChild(scrumnameP);
+	divInfo.appendChild(productownerP);
+	insertAfter(elementNav, divInfo);
+	
+}
+
+
+function showSprintInfoOneByOne(Position){
+	var elementdiv = document.getElementById("infoSprints");	
+	var divSprint = document.createElement("div");
+	divSprint.setAttribute("class", "SprintClick");
+	var clickClass=Position+"SprintDIV";
+	var textSprint = document.createElement("p");
+	var text = document.createTextNode("Sprint "+arraySprint[Position][0]);
+	textSprint.setAttribute("class", "SprintLetters");
+	textSprint.appendChild(text);
+	if (arraySprint[Position][4]==1) {
+		divSprint.style.backgroundColor = "green";
+	}else{
+		divSprint.style.backgroundColor = "grey";
+		}
+	textSprint.addEventListener("click", function(){
+		var x = document.getElementById(clickClass);
+		  if (x.style.display === "none") {
+		    x.style.display = "block";
+		  } else {
+		    x.style.display = "none";
+		  }
+	}); 
+	divSprint.appendChild(textSprint);
+
+	var divPSprint = document.createElement("div");
+	divPSprint.setAttribute("class", "infoSprint");
+	divPSprint.setAttribute("id",clickClass)
+
+	var hours = document.createElement("p");
+	var texth= document.createTextNode("Horas: "+arraySprint[Position][1]);
+	hours.appendChild(texth);
+	divPSprint.appendChild(hours);
+
+	var startDate = document.createElement("p");
+	var textsd= document.createTextNode("Fecha de inicio: "+arraySprint[Position][2]);
+	startDate.appendChild(textsd);
+	divPSprint.appendChild(startDate);
+
+	var endDate = document.createElement("p");
+	var texteD= document.createTextNode("Fecha de fin: "+arraySprint[Position][3]);
+	endDate.appendChild(texteD);
+	divPSprint.appendChild(endDate);
+
+	for (var hw = 0; hw < arrayHW.length; hw++) {
+		if (arrayHW[hw][3]==arraySprint[Position][5]) {
+			var elementList = document.createElement("p");
+			elementList.innerText=(arrayHW[hw][4]+". "+arrayHW[hw][1]+" "+arrayHW[hw][2]+"h");
+			elementList.setAttribute("class", "OneHomework");
+			elementList.setAttribute("orderHW", arrayHW[hw][4]);
+			divPSprint.appendChild(elementList);
+		}
+		
+	}
+	if(tipo==2){
+		var AddButton = document.createElement("input");
+		AddButton.setAttribute("id", "buttonAdd");
+		var Task = document.createElement("input");
+		AddButton.addEventListener("click", addTask);
+		AddButton.setAttribute("type", "image");
+		AddButton.setAttribute("src", "images/add.jpeg");
+		AddButton.setAttribute("width", "25");
+		AddButton.setAttribute("height", "25");
+		AddButton.setAttribute("align", "right");
+
+		Task.setAttribute("type", "text");
+		Task.setAttribute("class", "input-text");
+		divPSprint.appendChild(AddButton);
+		divPSprint.appendChild(Task);
+	}
+
+			
+	elementdiv.appendChild(divSprint);			
+	elementdiv.appendChild(divPSprint);
+
+}
+
+
+
+		
+function showSprintInfo(){
+	if (arraySprint!==null || arrayHW!==null){
+		var elementdiv = document.getElementsByTagName("div")[0];
+		var divSprints = document.createElement("div");
+		divSprints.setAttribute("id", "infoSprints");
+		insertAfter(elementdiv,divSprints);
+		for (var i = 0; i< arraySprint.length; i++) {
+			showSprintInfoOneByOne(i);
+
+		} 
+	}
+}
+
+function addTask(){
+
+	var myListP = document.getElementsByClassName("OneHomework")[2];
+	var contentTask = document.getElementsByTagName("input")[1].value;
+	var newContentP = document.createElement("p");
+	newContentP.setAttribute("class", "OneHomework");
+	var newContentTask = document.createTextNode(contentTask);
+	newContentP.appendChild(newContentTask);
+	insertAfter(myListP, newContentP);
+
+}	
